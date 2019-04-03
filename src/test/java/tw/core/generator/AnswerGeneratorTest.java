733695e12cbbc22f91eeb834629307e0b030fee9
qinshuqian <524477767@qq.com>
2019-04-03 17:00:15 +0800

    test AnswerGenerator

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,31 @@
 package tw.core.generator;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
+    private AnswerGenerator answerGenerator;
+    RandomIntGenerator randomIntGenerator;
 
+    @Before
+    public void setup(){
+        randomIntGenerator = new RandomIntGenerator();
+        answerGenerator = new AnswerGenerator(randomIntGenerator);
+    }
+
+    @Test
+    public void test_answer_generator(){
+        try {
+            answerGenerator.generate();
+        } catch (OutOfRangeAnswerException e) {
+            assertEquals(e.getMessage(),"Can't ask for more numbers than are available");
+        }
+    }
 }
 
