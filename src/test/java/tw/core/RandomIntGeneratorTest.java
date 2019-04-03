2a7786a4feb2971b3ffc47c7476a8d2801f1a33f
qinshuqian <524477767@qq.com>
2019-04-03 15:13:43 +0800

    test RandomIntGenerator

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,31 @@
 package tw.core;
 
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
+    private RandomIntGenerator randomIntGenerator;
+
+    @Before
+    public void setup(){
+        randomIntGenerator = new RandomIntGenerator();
+    }
+
+    @Test
+    public void when_nums_illegal(){
+        try {
+            randomIntGenerator.generateNums(2,9);
+        }catch (IllegalArgumentException e){
+            assertEquals(e.getMessage(),"Can't ask for more numbers than are available");
+        }
+
+    }
 
 }
